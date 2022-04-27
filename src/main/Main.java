97b9694101ba38b916a5e88c08de725e93efe8a4
Yutong Wang <19722030@bjtu.edu.cn>
2022-04-27 11:39:25 +0800

    Format.

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,67 +4,63 @@ import db.DbUser;
 
 public class Main {
 
-DbUser myDbUser = null;
+    DbUser myDbUser = null;
 
+    private void go() {
+        String lecName = null;
+        System.out.println("In go...");
+        myDbUser = new DbUser("University.db");
+        myDbUser.doQuery_1_a();
+        myDbUser.doQuery_1_b();
 
-private void go()
-{
-	String lecName = null;
-	System.out.println("In go...");
-	myDbUser = new DbUser("University.db");
-	myDbUser.doQuery_1_a();
-	myDbUser.doQuery_1_b();
+        myDbUser.doQuery_2_a();
+        myDbUser.doQuery_2_b();
 
-	myDbUser.doQuery_2_a();
-	myDbUser.doQuery_2_b();
+        myDbUser.doQuery_3_a();
+        myDbUser.doQuery_3_b();
 
-	myDbUser.doQuery_3_a();
-	myDbUser.doQuery_3_b();
+        myDbUser.doQuery_4_a();
+        myDbUser.doQuery_4_b();
+        myDbUser.doQuery_4_c();
+        myDbUser.doQuery_3_b();
 
-	myDbUser.doQuery_4_a();
-	myDbUser.doQuery_4_b();
-	myDbUser.doQuery_4_c();
-	myDbUser.doQuery_3_b();
+        myDbUser.doQuery_5_a();
+        myDbUser.doQuery_5_b();
 
-	myDbUser.doQuery_5_a();
-	myDbUser.doQuery_5_b();
+        myDbUser.parameterise_3_a("Mariani");
 
+        myDbUser.doQuery_6();
 
-	myDbUser.parameterise_3_a("Mariani");
+        myDbUser.execute_7c();
+        myDbUser.execute_7a();
+        myDbUser.execute_7b();
 
-	myDbUser.doQuery_6();
+        myDbUser.doQuery_6();
 
-	myDbUser.execute_7c();
-	myDbUser.execute_7a();
-	myDbUser.execute_7b();
+        System.out.println("Processing over");
 
-	myDbUser.doQuery_6();	
+        // This piece of code allows you to read in the surname of a lecturer at
+        // run-time and use it as the parameter
+        // to parameterise_3_a, but there is no need to use this code unless you want
+        // to.
 
-	System.out.println("Processing over");
+        // BufferedReader brin = new BufferedReader(new InputStreamReader(System.in));
+        // System.out.print("type in surname of lecturer: ");
+        // try {
+        // lecName = brin.readLine();
+        // } catch (IOException e) {
+        // System.out.println("Main.go() : Failure in I/O" + e);
+        // }
+        // ;
 
-/*
+        // parameterise_3_a(lecName);
 
-//   This piece of code allows you to read in the surname of a lecturer at run-time and use it as the parameter
-//   to parameterise_3_a, but there is no need to use this code unless you want to.
+        // myDbUser.close();
+    }; // end of method "go"
 
-	BufferedReader brin = new BufferedReader(new InputStreamReader(System.in));
-      System.out.print("type in surname of lecturer: ");
-      try {
-           lecName = brin.readLine();
-           }
-           catch (IOException e) {
-			System.out.println("Main.go() : Failure in I/O"+e);
-		};
-
-	parameterise_3_a(lecName);
-*/
-	myDbUser.close();
-}; // end of method "go"
-
-public static void main(String [ ] args)
-{
-	Main myMain = new Main();
-	myMain.go();
-} // end of method "main"
+    public static void main(String[] args) {
+        Main myMain = new Main();
+        myMain.go();
+    } // end of method "main"
 
 } // end of class "Main"
